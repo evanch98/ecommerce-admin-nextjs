2813901af269e3778f50bc4284624ee713651126
Kyaw Thu <evanch98@gmail.com>
2023-07-28 00:47:08 +0800

    add a new component StoreModal

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Create Store Form
+    </Modal>
+  );
+};
