307c24845f8e933403b4d6b401d06aedfb5ea23e
Kyaw Thu <evanch98@gmail.com>
2023-07-27 23:36:09 +0800

    testing the Button component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>Admin Dashboard</p>;
+  return (
+    <div className="p-4">
+      <Button size="sm" variant="destructive">Click Me</Button>
+    </div>
+  );
 }
