37b936856f6baab1d25ff1242efa2f097a3f7fe7
Kyaw Thu <evanch98@gmail.com>
2023-07-28 00:58:22 +0800

    update the setupPage component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal
-        isOpen={true}
-        onClose={() => {}}
-        title="Test"
-        description="Test Description"
-      >
-        Children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div className="p-4">Root Page</div>;
 };
 
 export default SetupPage;
