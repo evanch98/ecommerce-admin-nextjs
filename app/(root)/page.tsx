4153d32206a625c4bf0939af7da0b06dc6c299ec
Kyaw Thu <evanch98@gmail.com>
2023-07-28 00:12:38 +0800

    update the SetupPage component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button size="sm" variant="destructive">
-        Click Me
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
