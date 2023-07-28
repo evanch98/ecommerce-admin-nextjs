dc845290a7706da46b7b3a78378232117dde1604
Kyaw Thu <evanch98@gmail.com>
2023-07-29 00:11:40 +0800

    update the Navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,19 @@
 "use client";
 
+import { UserButton } from "@clerk/nextjs";
+
 const Navbar = () => {
-  return <div>Nav Bar Component</div>;
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>Store Switcher</div>
+        <div>Routes</div>
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
