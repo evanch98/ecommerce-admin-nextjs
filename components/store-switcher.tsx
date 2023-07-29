65fe7eba3db86421d85e99212b94a2d832dd4164
Kyaw Thu <evanch98@gmail.com>
2023-07-29 14:11:21 +0800

    update the StoreSwitcher component

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,3 +1,40 @@
-export default function StoreSwitcher() {
+"use client";
+
+import { PopoverTrigger } from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Store } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
+
+export default function StoreSwitcher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
+  const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }));
+
+  const currenStore = formattedItems.find(
+    (item) => item.value === params.storeId
+  );
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setOpen(false);
+    router.push(`/${store.value}`);
+  };
   return <div>Store Switcher</div>;
 }
