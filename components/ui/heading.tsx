e4f705bcc27322ddef8c79002c622d0da102a5a2
Kyaw Thu <evanch98@gmail.com>
2023-07-30 12:28:10 +0800

    update the Heading component

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -6,8 +6,8 @@ interface HeadingProps {
 export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
+      <p className="text-sm text-muted-foreground">{description}</p>
     </div>
   );
 };
