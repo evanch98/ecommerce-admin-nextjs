edab2683a17080834daa7fa2c089b4a00439633e
Kyaw Thu <evanch98@gmail.com>
2023-07-30 12:21:51 +0800

    update the SettingsPage

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -29,7 +29,7 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <SettingsForm />
+        <SettingsForm initialData={store} />
       </div>
     </div>
   );
