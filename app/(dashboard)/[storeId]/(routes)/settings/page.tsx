c6e5152c9078846b6154cbe90b3a11adf4463dce
Kyaw Thu <evanch98@gmail.com>
2023-07-30 12:20:12 +0800

    update the SettingsPage

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,6 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { SettingsForm } from "./components/settings-form";
 
 interface SettingsPageProps {
   params: {
@@ -27,7 +28,9 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
   }
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">Hello Settings</div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm />
+      </div>
     </div>
   );
 };
