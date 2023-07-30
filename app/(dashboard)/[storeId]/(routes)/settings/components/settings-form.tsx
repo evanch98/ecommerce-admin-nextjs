9756da671883ad202939af45251f9b44a7d9108e
Kyaw Thu <evanch98@gmail.com>
2023-07-30 23:35:02 +0800

    update the SettingsForm component

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,16 +1,32 @@
 "use client";
 
+import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type settingsFormValues = z.infer<typeof formSchema>;
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const form = useForm<settingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
   return (
     <>
       <div className="flex items-center justify-between">
