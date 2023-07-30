0ad948f39e0431216f8ed4e808d2eb949aa3fbda
Kyaw Thu <evanch98@gmail.com>
2023-07-30 12:16:03 +0800

    update the SettingsPage component

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,5 +1,35 @@
-const SettingsPage = () => {
-  return <div>Hello Settings</div>;
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface SettingsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">Hello Settings</div>
+    </div>
+  );
 };
 
 export default SettingsPage;
