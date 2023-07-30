79d6537a6a98907a69ce9558cef3b6ac115ee9dc
Kyaw Thu <evanch98@gmail.com>
2023-07-30 12:21:16 +0800

    update the SettingForm component

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,3 +1,11 @@
-export const SettingsForm = () => {
+"use client";
+
+import { Store } from "@prisma/client";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   return <div>Settings Form</div>;
 };
