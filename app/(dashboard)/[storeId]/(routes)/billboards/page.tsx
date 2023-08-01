6fcb33a57cfc861e88eefd649c89621fa4a56719
Kyaw Thu <evanch98@gmail.com>
2023-08-01 11:11:00 +0800

    update teh BillboardsPage

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,5 +1,11 @@
 const BillboardsPage = () => {
-  return <div>Billboards</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient />
+      </div>
+    </div>
+  );
 };
 
 export default BillboardsPage;
