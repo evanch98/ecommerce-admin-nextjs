b6208ed69d1c2054a367829cc9884e4122114db3
Kyaw Thu <evanch98@gmail.com>
2023-08-01 11:18:10 +0800

    update the BillboardClient component

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -4,8 +4,12 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
 export const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -13,7 +17,9 @@ export const BillboardClient = () => {
           title="Billboards (0)"
           description="Manage billboards for your store"
         />
-        <Button>
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <Plus className="mr-2 h-4 w-4" />
           Add New
         </Button>
