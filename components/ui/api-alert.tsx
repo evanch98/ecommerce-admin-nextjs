7a39c8001bc409eb983ffc242f6e78c4a2b9ed87
Kyaw Thu <evanch98@gmail.com>
2023-08-01 10:18:45 +0800

    add a new component ApiAlert

---
+++ components/ui/api-alert.tsx
@@ -0,0 +1,30 @@
+import { Alert } from "@/components/ui/alert";
+import { Server } from "lucide-react";
+
+interface ApiAlertProps {
+  title: string;
+  description: string;
+  variant: "public" | "admin";
+}
+
+const textMap: Record<ApiAlertProps["variant"], string> = {
+  public: "Public",
+  admin: "Admin",
+};
+
+const variantMap: Record<ApiAlertProps["variant"], string> = {
+  public: "secondary",
+  admin: "destructive",
+};
+
+export const ApiAlert: React.FC<ApiAlertProps> = ({
+  title,
+  description,
+  variant = "public",
+}) => {
+  return (
+    <Alert>
+      <Server className="h-4 w-4" />
+    </Alert>
+  );
+};
