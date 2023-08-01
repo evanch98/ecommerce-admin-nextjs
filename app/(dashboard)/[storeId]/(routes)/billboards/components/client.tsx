32978fa2ebeb688dbb3c1e975868c896306a7014
Kyaw Thu <evanch98@gmail.com>
2023-08-01 11:15:56 +0800

    update the BillboardClient component

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,9 +1,24 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+
 export const BillboardClient = () => {
   return (
     <>
-      <div className="flex items-center justify-between">Billboard Client</div>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Billboards (0)"
+          description="Manage billboards for your store"
+        />
+        <Button>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
     </>
   );
 };
