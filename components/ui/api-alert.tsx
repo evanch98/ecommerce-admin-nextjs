540cf5b179f336367ad7662857cf076f7bbbec85
Kyaw Thu <evanch98@gmail.com>
2023-08-01 10:30:05 +0800

    update the ApiAlert component

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -1,5 +1,8 @@
-import { Alert, AlertTitle } from "@/components/ui/alert";
-import { Server } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Badge, BadgeProps } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { CopyIcon, Server } from "lucide-react";
+import toast from "react-hot-toast";
 
 interface ApiAlertProps {
   title: string;
@@ -12,7 +15,7 @@ const textMap: Record<ApiAlertProps["variant"], string> = {
   admin: "Admin",
 };
 
-const variantMap: Record<ApiAlertProps["variant"], string> = {
+const variantMap: Record<ApiAlertProps["variant"], BadgeProps["variant"]> = {
   public: "secondary",
   admin: "destructive",
 };
@@ -22,10 +25,25 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
   description,
   variant = "public",
 }) => {
+  const onCopy = () => {
+    navigator.clipboard.writeText(description);
+    toast.success("API Route copied to the clipboard.");
+  };
   return (
     <Alert>
       <Server className="h-4 w-4" />
-      <AlertTitle className="flex items-center gap-x-2">{title}</AlertTitle>
+      <AlertTitle className="flex items-center gap-x-2">
+        {title}
+        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+      </AlertTitle>
+      <AlertDescription className="mt-4 flex items-center justify-between">
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+          {description}
+        </code>
+        <Button variant="outline" size="icon" onClick={onCopy}>
+          <CopyIcon className="h-4 w-4" />
+        </Button>
+      </AlertDescription>
     </Alert>
   );
 };
