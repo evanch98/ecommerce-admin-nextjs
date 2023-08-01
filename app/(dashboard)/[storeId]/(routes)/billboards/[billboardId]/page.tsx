cd383bb9323186c65d725b0bde8a95eb4d7bc0b3
Kyaw Thu <evanch98@gmail.com>
2023-08-01 11:29:26 +0800

    update the BillboardPage

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,5 +1,23 @@
-const BillboardPage = () => {
-  return <div>This is a form for billboards</div>;
+import prismadb from "@/lib/prismadb";
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        
+      </div>
+    </div>
+  );
 };
 
 export default BillboardPage;
