54b0b46f70cf28b17fdb2ac2b52ac002a4e15930
Kyaw Thu <evanch98@gmail.com>
2023-08-01 10:45:21 +0800

    update the ApiAlert component

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Badge, BadgeProps } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
