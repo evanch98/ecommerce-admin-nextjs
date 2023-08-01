4494dfca3bc99aafc06544f6150300420e357895
Kyaw Thu <evanch98@gmail.com>
2023-08-01 10:00:21 +0800

    update the MainNav component

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,6 +13,11 @@ export function MainNav({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
