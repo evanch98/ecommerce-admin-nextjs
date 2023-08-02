cadc9870126e81e773542e51eff5fab046139a00
Kyaw Thu <evanch98@gmail.com>
2023-08-02 13:25:33 +0800

    add a new POST function

---
+++ app/api/[storeId]/billboards/route.ts
@@ -0,0 +1,55 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storedId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!label) {
+      return new NextResponse("Label is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Image URL is required", { status: 400 });
+    }
+
+    if (!params.storedId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storedId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.create({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storedId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
