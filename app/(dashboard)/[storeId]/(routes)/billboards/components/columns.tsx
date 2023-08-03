fa1882d380d2c5acf62753ab960a07afc62846c8
Kyaw Thu <evanch98@gmail.com>
2023-08-03 17:16:50 +0800

    add columns

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+export type BillboardColumn = {
+  id: string;
+  label: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<BillboardColumn>[] = [
+  {
+    accessorKey: "label",
+    header: "Label",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+  },
+];
