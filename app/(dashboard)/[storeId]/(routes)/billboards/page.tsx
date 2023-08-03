78caa6980ea7d64487807ec8652a7797f64691f6
Kyaw Thu <evanch98@gmail.com>
2023-08-03 17:05:16 +0800

    update the BillboardPage

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,19 @@
+import prismadb from "@/lib/prismadb";
 import { BillboardClient } from "./components/client";
 
-const BillboardsPage = () => {
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={billboards} />
       </div>
     </div>
   );
