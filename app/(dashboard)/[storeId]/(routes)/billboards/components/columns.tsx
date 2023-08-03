77fde6938d634288d08a4fc8076d73f615b02878
Kyaw Thu <evanch98@gmail.com>
2023-08-03 18:32:48 +0800

    update the columns.tsx

--- app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
 
 export type BillboardColumn = {
   id: string;
@@ -16,5 +17,9 @@ export const columns: ColumnDef<BillboardColumn>[] = [
   {
     accessorKey: "createdAt",
     header: "Created At",
+  },
+  {
+    id: "actions",
+    cell: () => <CellAction />,
   },
 ];
