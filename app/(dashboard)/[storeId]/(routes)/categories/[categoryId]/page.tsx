f6a35509ec9322ec7d4621234ab28698f7bad333
Kyaw Thu <evanch98@gmail.com>
2023-08-04 15:43:23 +0800

    update the CategoryPage

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -4,7 +4,7 @@ import { CategoryForm } from "./components/category-form";
 const CategoryPage = async ({
   params,
 }: {
-  params: { categoryId: string };
+  params: { categoryId: string; storeId: string };
 }) => {
   const category = await prismadb.category.findUnique({
     where: {
@@ -12,10 +12,16 @@ const CategoryPage = async ({
     },
   });
 
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <CategoryForm initialData={category} />
+        <CategoryForm billboards={billboards} initialData={category} />
       </div>
     </div>
   );
