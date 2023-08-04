a9b742a03f1dc32ddcf5f71b460ae5c07b5e8d5d
Kyaw Thu <evanch98@gmail.com>
2023-08-04 11:40:39 +0800

    update the ApiList component

--- components/ui/api-list.tsx
+++ components/ui/api-list.tsx
@@ -1,5 +1,30 @@
 "use client";
 
-export const ApiList = () => {
-  return <div>Api list</div>;
+import { useOrigin } from "@/hooks/use-origin";
+import { useParams } from "next/navigation";
+import { ApiAlert } from "@/components/ui/api-alert";
+
+interface ApiListProps {
+  entityName: string;
+  entityIdName: string;
+}
+
+export const ApiList: React.FC<ApiListProps> = ({
+  entityName,
+  entityIdName,
+}) => {
+  const params = useParams();
+  const origin = useOrigin();
+
+  const baseUrl = `${origin}/api/${params.storeId}`;
+
+  return (
+    <>
+      <ApiAlert
+        title="GET"
+        variant="public"
+        description={`${baseUrl}/${entityName}`}
+      />
+    </>
+  );
 };
