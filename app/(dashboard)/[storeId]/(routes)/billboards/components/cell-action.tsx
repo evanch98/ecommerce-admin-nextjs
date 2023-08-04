00fe1f4e709789ab3727a2a1343ebc16db1471e6
Kyaw Thu <evanch98@gmail.com>
2023-08-04 11:03:47 +0800

    update the CellAction component

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,3 +1,11 @@
-export const CellAction = () => {
+"use client";
+
+import { BillboardColumn } from "./columns";
+
+interface CellActionProps {
+  data: BillboardColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
   return <div>CellAction</div>;
 };
