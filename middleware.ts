d8362201fb93b31041e17f0065ff5226c96d1285
Kyaw Thu <evanch98@gmail.com>
2023-08-04 11:52:02 +0800

    update the middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 // to protect all routes
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/:path*"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
