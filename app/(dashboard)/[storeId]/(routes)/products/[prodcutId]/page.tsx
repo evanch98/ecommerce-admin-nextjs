8fe27dde042452f4106b023e176cd40729521f1d
Kyaw Thu <evanch98@gmail.com>
2023-08-07 21:21:31 +0800

    add ProductPage

--- app/(dashboard)/[storeId]/(routes)/products/[prodcutId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[prodcutId]/page.tsx
@@ -1,24 +1,24 @@
 import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
-const BillboardPage = async ({
+const ProductPage = async ({
   params,
 }: {
-  params: { billboardId: string };
+  params: { productId: string };
 }) => {
-  const billboard = await prismadb.billboard.findUnique({
+  const product = await prismadb.product.findUnique({
     where: {
-      id: params.billboardId,
+      id: params.productId,
     },
   });
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardForm initialData={billboard} />
+        <BillboardForm initialData={product} />
       </div>
     </div>
   );
 };
 
-export default BillboardPage;
+export default ProductPage;
