250753b81ac34046e42fe78573daabb00278bd9e
Kyaw Thu <evanch98@gmail.com>
2023-08-07 13:45:46 +0800

    add currency formatter

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
