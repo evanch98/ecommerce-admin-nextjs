ea8ebef877a95eb39b47ca1a146398b5e2fdb766
Kyaw Thu <evanch98@gmail.com>
2023-08-08 23:01:57 +0800

    update the ProductPage

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,7 +1,11 @@
 import prismadb from "@/lib/prismadb";
 import { ProductForm } from "./components/product-form";
 
-const ProductPage = async ({ params }: { params: { productId: string } }) => {
+const ProductPage = async ({
+  params,
+}: {
+  params: { productId: string; storedId: string };
+}) => {
   const product = await prismadb.product.findUnique({
     where: {
       id: params.productId,
@@ -11,10 +15,33 @@ const ProductPage = async ({ params }: { params: { productId: string } }) => {
     },
   });
 
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storedId,
+    },
+  });
+
+  const sizes = await prismadb.size.findMany({
+    where: {
+      storeId: params.storedId,
+    },
+  });
+
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storedId,
+    },
+  });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm initialData={product} />
+        <ProductForm
+          initialData={product}
+          categories={categories}
+          colors={colors}
+          sizes={sizes}
+        />
       </div>
     </div>
   );
