4b9e02642377ef7286c70ef353cf823af4abc5f5
Kyaw Thu <evanch98@gmail.com>
2023-08-23 12:29:23 +0800

    update the GET function

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -15,6 +15,9 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
     return NextResponse.json(category);
   } catch (error) {
