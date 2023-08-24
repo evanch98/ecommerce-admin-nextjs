fce19f1597d0c76a2263db48da160338c482101f
Kyaw Thu <evanch98@gmail.com>
2023-08-24 14:45:55 +0800

    update the DashboardPage

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,22 @@
-const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = ({ params }) => {
+  return (
+    <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
